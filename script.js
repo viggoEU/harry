f4e690c95746d530f4c6441f3287247d6121e95b
viggoEU <161716014+viggoEU@users.noreply.github.com>
2026-06-13 16:39:54 +0200

    Update script.js
    changes from vscode

--- script.js
+++ script.js
@@ -14,4 +14,19 @@ sections.forEach(section => {
   });
 });
 
+const preview = document.getElementById("image-preview");
+const previewImg = preview.querySelector("img");
+
+document.querySelectorAll(".preview-link").forEach(link => {
+
+    link.addEventListener("mouseenter", () => {
+
+        previewImg.src = link.dataset.image;
+
+        preview.style.display = "block";
+    });
+
+    link.addEventListener("mouseleave", () => {
+        preview.style.display = "none";
+    });
 });
